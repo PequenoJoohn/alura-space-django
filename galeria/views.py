3b7cb144ad17f3f680516fba2b5a4833b5cce812
Jonathan Matos <jonathan.matos2445@gmail.com>
2024-12-13 18:38:00 -0300

    feat: add login and register

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from galeria.models import Fotografia
+from django.contrib import messages
 
 def index(request):
     # return HttpResponse("<h1>Alura Space</h1>")
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicado=True)
     return render(request, 'galeria/index.html', {"cards": fotografias})
 
@@ -11,6 +16,11 @@ def imagem (request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar (request):
+    
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuário não logado')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("-data_fotografia").filter(publicado=True)
     
     if "buscar" in request.GET:
--- usuarios/views.py
+++ usuarios/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms
 from django.contrib.auth.models import User
 from django.contrib import auth
+from django.contrib import messages
+
 # Create your views here.
 def login(request):
     form = LoginForms()
@@ -10,7 +12,7 @@ def login(request):
         form = LoginForms(request.POST)
         
         if form.is_valid():
-            nome = form['username'].value()
+            nome = form['nome_login'].value()
             senha = form['senha'].value()
             
             usuario = auth.authenticate(
@@ -21,8 +23,10 @@ def login(request):
             
             if usuario is not None:
                 auth.login(request, usuario)
+                messages.success(request, f"{nome} logado com sucesso!")
                 return redirect('index')
             else:
+                messages.error(request, "Erro ao efetuar login")
                 return redirect("login")
     
     return render(request, 'usuarios/login.html', { "form": form })
@@ -35,6 +39,7 @@ def cadastro(request):
         
         if form.is_valid():
             if form["senha_1"].value() != form['senha_2'].value():
+                messages.error(request, 'Senhas não são iguais')
                 return redirect('cadastro')
             
             nome=form["nome_cadastro"].value()
@@ -42,6 +47,7 @@ def cadastro(request):
             senha=form["senha_1"].value()
             
             if User.objects.filter(username=nome).exists():
+                messages.error(request, 'Usuário já existente')
                 return redirect('cadastro')
             
             usuario = User.objects.create_user(
@@ -50,8 +56,14 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, 'Cadastro efetuado com sucesso!')
             return redirect("login")
             
         
     
     return render(request, 'usuarios/cadastro.html', { "form": form })
+
+def logout(request):
+    auth.logout(request)
+    messages.success(request, "Logout efetuado com sucesso!")
+    return redirect('login')
